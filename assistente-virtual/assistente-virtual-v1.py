c7c66a111b01908dfdafedee8d7db8518d64a303
André Alves <112015245+andre-f-alves@users.noreply.github.com>
2022-10-11 10:49:09 -0300

    Atualização e documentação do código

--- assistente-virtual/assistente-virtual-v1.py
+++ assistente-virtual/assistente-virtual-v1.py
@@ -1,7 +1,12 @@
 import microfone as mic
 import pyautogui as pg
 
-pg.PAUSE = 1
+""" 
+    * No momento, a utilização do assistente está limitada a inicialização de alguns programas
+    por meio da ferramenta Executar do sistema operacional Windows.
+"""
+
+pg.PAUSE = 1 # -> Define o tempo de espera entre a execução de tarefas automáticas (1s).
 
 programas = {
     'word': 'winword',
@@ -11,16 +16,29 @@ programas = {
     'edge': 'msedge',
     'chrome': 'chrome'
 }
+
+""" 
+    Ativa o microfone para a captura do áudio e divide o texto retornado,
+    transformando-o em uma lista (list()).
+"""
 user = mic.escutar()
 txtlist = str(user).lower().split(' ')
 print(txtlist)
 
 comando = ''
 for palavra in txtlist:
+    """
+    Percorre todas as palavras da lista criada, verificando se alguma
+    se encontra no dicionário 'programas' (dict()).
+    """
     if palavra in programas:
         comando = palavra
         break
 
+"""
+    De forma automática, abre o Executar, indica o programa que será iniciado
+    por meio do comando definido em 'programas' e pressiona Enter para iniciá-lo.
+"""
 pg.hotkey('win', 'r')
 pg.write(programas[comando])
 pg.press('enter')
--- assistente-virtual/microfone.py
+++ assistente-virtual/microfone.py
@@ -2,27 +2,41 @@ import speech_recognition as sr
 
 
 def identificar_microfones():
+    """ 
+    Exibe uma lista dos microfones conectados à máquina.
+    """
     for indice, nome in enumerate(sr.Microphone.list_microphone_names()):
+        """ 
+        Percorre a lista de microfones conectados, exibindo seu índice
+        (passado para a função 'escutar()', indicando qual microfone será utilizado) e o nome.
+        """
         print(f'Índice: {indice} = Microfone - {nome}')
 
 
-def fone_de_ouvido():
-    microfone = 0
-    for indice, nome in enumerate(sr.Microphone.list_microphone_names()):
-        if nome == 'Headset (i12 Hands-Free AG Audi':
-            microfone = indice
-            break
-    return microfone
-
-
-def escutar(microfone=0, fone=True):
+def escutar(microfone=0):
+    """ 
+    Realiza o reconhecimento da fala por meio da captura do áudio do microfone
+    e da remoção de ruídos de fundo, utilizando a biblioteca Speech Recognition.
+    Depois, transforma o áudio capturado em texto com a ferramenta de reconhecimento do Google.
+    
+    Parêmtros
+    =-=-=-=-=-=-=
+    microfone: indica o índice do microfone conectado que será utilizado.
+    
+    '''
+    Retorno
+    =-=-=-=-=-=-=
+    Retorna o áudio em formato de texto.
+    """
     rec = sr.Recognizer()
-
+    
     indice = microfone
-    if fone:
-        indice = fone_de_ouvido()
-
     with sr.Microphone(device_index=indice) as mic:
+        """ 
+        Declara uma variável, uma forma abreviada, para utilizar o microfone indicado.
+        Com isso, inicializa a captura do áudio, ajusta-o e o transforma em texto.
+        No final do blocoo de código, desliga o microfone e retorna o texto.
+        """
         rec.adjust_for_ambient_noise(mic)
         print('Ao seu dispor, senhor!')
 
